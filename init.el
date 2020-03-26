fe68a17e686dc957eff5e00f6745f32ab4337119
Saul Blanco Tejero <saul.blanco.tejero@iesjulianmarias.es>
2020-03-26 23:34:08 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,46 @@
+;; MUST STUFF
+;; Source repos
+(require 'package)
+(setq package-enable-at-startup nil)
+
+(setq package-archives '(("elpa" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+(package-initialize)
+
+;; CONFIG STUFF
+;; Bootstrapping use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Load configuration
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+
+;; Set default font
+(set-face-attribute 'default nil
+                    :family "Fira Code"
+                    :height 120
+                    :weight 'normal
+                    :width 'normal)
+
+;; CUSTOM STUFF
+;; By Custom
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(display-line-numbers (quote relative))
+ '(package-selected-packages
+   (quote
+    (ng2-mode which-key use-package treemacs dashboard dracula-theme))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
