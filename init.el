1c0fc00b5a853bb41161d1f8fef5813902e67455
Saul Blanco Tejero <saul.blanco.tejero@iesjulianmarias.es>
2020-03-29 21:17:36 +0200

    Swiper added and some fixes

--- init.el
+++ init.el
@@ -36,7 +36,7 @@
  '(display-line-numbers (quote relative))
  '(package-selected-packages
    (quote
-    (popup-kill-ring company rainbow-delimiters ng2-mode which-key use-package treemacs dashboard dracula-theme))))
+    (htmlize swiper ox-twbs popup-kill-ring company rainbow-delimiters ng2-mode which-key use-package treemacs dashboard dracula-theme))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
